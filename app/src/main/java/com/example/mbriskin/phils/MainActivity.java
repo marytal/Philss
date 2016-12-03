e67dbc8d637b2b2dbab597544da0a05bfc40b12b
Mary Briskin <mary-briskin@hotmail.com>
2016-12-03 17:09:57 -0500

    Integrate FB login add button

--- app/src/main/java/com/example/mbriskin/phils/MainActivity.java
+++ app/src/main/java/com/example/mbriskin/phils/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.mbriskin.phils;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.activity_main);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
